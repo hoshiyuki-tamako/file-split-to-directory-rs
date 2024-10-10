830a34430dab0bbfcfc263c444b3b6ab02c74f6b
星雪靈兒 <icestarsnowy@gmail.com>
2024-10-10 19:05:18 +0800

    fix filename sort issue

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use anyhow::Result;
 use clap::Parser;
-use human_sort::compare;
 use itertools::Itertools;
 use std::fs;
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
-#[command(version = "0.0.1", about = "Split files to directory", long_about = None)]
+#[command(version, about, long_about = None)]
 struct Args {
     #[arg(required = true)]
     path: String,
@@ -21,22 +20,21 @@ struct Args {
 }
 
 impl Args {
-    pub fn to_path_buf(&self) -> PathBuf {
+    pub fn path_buf(&self) -> PathBuf {
         PathBuf::from(self.path.as_str())
     }
 }
 
 fn main() -> Result<()> {
     let args: Args = Args::parse();
-    split_files(&args.to_path_buf(), args.chunk)?;
+    split_files(&args.path_buf(), args.chunk)?;
     Ok(())
 }
 
 fn split_files(root: &PathBuf, chunk: usize) -> Result<()> {
     let chunks = fs::read_dir(root)?
         .enumerate()
-        .filter(|(_, f)| f.is_ok())
-        .map(|(_, f)| f.unwrap())
+        .filter_map(|(_, f)| f.ok())
         .filter(|f| {
             let Ok(t) = f.file_type() else {
                 return false;
@@ -44,7 +42,7 @@ fn split_files(root: &PathBuf, chunk: usize) -> Result<()> {
             t.is_file()
         })
         .sorted_by(|a, b| {
-            compare(
+            natord::compare(
                 &a.file_name().to_string_lossy(),
                 &b.file_name().to_string_lossy(),
             )
